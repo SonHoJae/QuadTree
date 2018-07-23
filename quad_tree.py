a7b5db7999285d9f8e5e160550fbafd094765b1f
hojae <sonhj07@gmail.com>
2018-07-24 05:36:38 +0900

    files managed

---
+++ quad_tree.py
@@ -0,0 +1,51 @@
+# Data set is from https://www.kaggle.com/max-mind/world-cities-database#worldcitiespop.csv
+import pickle
+
+import pandas as pd
+from collections import defaultdict
+
+def binary_direction(lat, long, left=-54.933333, right=82.483333, down=-179.9833333, up=180.0,  depth=1):
+    location = ''
+    for idx in range(depth):
+        if idx % 2 == 0:
+            mid = (left + right)/ 2
+            if lat > mid:
+                location +='1'
+                left = mid
+            else:
+                location += '0'
+                right = mid
+        else:
+            mid = (down + up) / 2
+            if long > mid:
+                location += '1'
+                down = mid
+            else:
+                location += '0'
+                up = mid
+    return location
+
+def data_preprocess():
+    df = pd.read_csv('data/worldcitiespop.csv')
+    df = df[['Region','Latitude','Longitude']]
+
+    right_most = max(df['Latitude'])
+    left_most = min(df['Latitude'])
+    up_most = max(df['Longitude'])
+    down_most = min(df['Longitude'])
+
+    quad_tree = defaultdict(set)
+
+    for index, row in df.iterrows():
+        quad_loc =binary_direction(row['Latitude'], row['Longitude'], left_most, right_most, down_most, up_most, depth=30)
+        quad_tree[row['Region']].add(quad_loc)
+
+    geo_index = {}
+    for k, v in quad_tree.items():
+        geo_index.update(
+            **dict.fromkeys(list(v),k)
+        )
+
+    f = open('quad_tree_30.pckl', 'wb')
+    pickle.dump(geo_index, f)
+    f.close()
---
+++ test.py
@@ -0,0 +1,24 @@
+import pickle
+import datetime
+import quad_tree
+f = open('data/quad_tree_30.pckl', 'rb')
+data = pickle.load(f)
+f.close()
+
+#   quad_tree.data_preprocess()
+
+try:
+    start = datetime.datetime.now()
+    print('start time -> ' + str(start))
+    for i in range(100002):
+        quad_seq = quad_tree.binary_direction(42.466667, 1.500000, depth=30)  # Region 6
+    print(data[quad_seq])
+    finish = datetime.datetime.now()
+    print('finish time -> ' + str(finish))
+    print('taking time -> ' +str(finish - start))
+except KeyError:
+    print('Key Error !')
+except Exception as e:
+    print('Something happened')
+    print(e)
+
